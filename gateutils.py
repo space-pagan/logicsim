e398dee8c02c0cc522a37dd933ecd328d344d091
Zoya Samsonov <zoya.samsonov@gmail.com>
2020-07-03 13:22:23 -0500

    Implements basic gates, latches, and some complex gate arrays.

---
+++ gateutils.py
@@ -0,0 +1,37 @@
+def booltobin(state):
+    state = bool(state)
+    return int(format(state, 'b'))
+
+def selffn(inputs, state, touched_list):
+    return state
+
+def andfn(inputs, state, touched_list):
+    out = True
+    for i in inputs:
+        out &= i.update(touched_list)
+    return out
+
+def orfn(inputs, state, touched_list):
+    out = False
+    for i in inputs:
+        out |= i.update(touched_list)
+    return out
+
+def notfn(inputs, state, touched_list):
+    return not inputs[0].update(touched_list)
+
+def srfn(inputs, state, touched_list):
+    S, R = inputs
+    if S.update(touched_list):
+        return True
+    if R.update(touched_list):
+        return False
+    return state
+
+def pulsefn(inputs, state, touched_list):
+    inp = inputs[0].update(touched_list)
+    if state[0]:
+        return [False, inp]
+    if state[1]:
+        return [False, inp]
+    return [inp, inp]
